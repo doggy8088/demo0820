d67eb050e41db9d74265a37117ec062c34fd2291
Will <doggy.huang@gmail.com>
2016-08-20 11:29:56 +0800

    09 隨堂測驗：請給予搜尋輸入框一個預設的關鍵字內容 "TEST"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  keyword = '';
+
+  keyword = 'TEST';
 
   doSearch($event: KeyboardEvent) {
     let input = $event.target as HTMLInputElement;
